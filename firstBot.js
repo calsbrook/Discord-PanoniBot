b70f70938446a365b737edbfed690784373608c8
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2018-07-19 04:09:11 -0700

    started adding tic tac toe. updated ping to display ping.

--- firstBot.js
+++ firstBot.js
@@ -1,28 +1,54 @@
-const Discord = require("discord.js");
-
-const bot = new Discord.Client();
-
-var ping = require('./ping');
+const Discord = require('discord.js');
+// const ttt = require('./ticTacToe')
+// var ping = require('./ping');
 var auth = require('./auth.json')
-
-bot.on("ready", () => {
+var dong = ':grinning:'
+const client = new Discord.Client();
+var X = 'X'
+client.on("ready", () => {
+	var emojis = {}
+	var hugeList = client.emojis.array()
 	console.log("I am ready!");
+	function addEmoji() {
+		// console.log('adding emoji')
+		// console.log(`this is the huge list ${hugeList[0]}`)
+		for (i=0; i ++; i === hugeList.length) {
+			emojis[hugeList[i].Emoji.name] = hugeList[i].Emoji.id
+		}
+		// console.log(emojis)
+	}
+	addEmoji();
 });
 
-bot.on("message", (message) =>
-{
-	ping();
-	var input = message.content.toUpperCase();
+
+client.on("message", async (message) => {
+	if (message.author.bot) return;
+
 	// makes sure the command works regardless of upper or lowercase
+	var input = message.content.toUpperCase();
 	
-	if(input === "HI")
-	{
+	if(input === "HI") {
 		message.channel.send("Hey wassup hello!");
 	}
 
-	if(input === "TIC TAC TOE") {
-		game();
+	if(input === '!PING') {
+		const m = await message.channel.send("Ping?");
+		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 	}
+
+	if(input === 'YESNO') {
+		message.channel.send('test')
+	}
+
+	if(input === "TIC TAC TOE") {
+		// console.log('tic tac toe started')
+		message.channel.send("`Tic tac toe started`")
+		// message.channel.send(` _ _ A _ B _ C\n1) ${X}`)
+		// ticTacToe();
+	}
+});
+
+function ticTacToe() {
 	var board = {};
 	var score = {};
 	var move;
@@ -31,72 +57,81 @@ bot.on("message", (message) =>
 	var winner;
 	
 	function game() {
-	//var p1 = prompt("Player 1")
-	//var p2 = prompt("Player 2")
-	console.log('game start')
-	init();
-	numGames = 1;
-	play();
+		//var p1 = prompt("Player 1")
+		//var p2 = prompt("Player 2")
+		console.log('game running')
+		init();
+		numGames = 1;
+		// play();
 	}
 	
 	function init() {
-	score = {
-		X: 0,
-		O: 0,
-		T: 0,
-	};
-	turn = 'X';
+		score = {
+			X: 0,
+			O: 0,
+			T: 0,
+		};
+		turn = 'X';
 	}
 	
 	function newGame() {
-	board = {
-		top: '     A   B   C',
-		line: '   -----------',
-		A1: ' ', A2: ' ', A3: ' ',
-		B1: ' ', B2: ' ', B3: ' ',
-		C1: ' ', C2: ' ', C3: ' '
-	};
+		board = {
+			top: '     A   B   C',
+			line: '   -----------',
+			A1: ' ', A2: ' ', A3: ' ',
+			B1: ' ', B2: ' ', B3: ' ',
+			C1: ' ', C2: ' ', C3: ' '
+		};
 	}
 	
 	function drawBoard() {
-	message.channel.send(`${board.top}\n
-	1) ${board.A1} | ${board.B1} | ${board.C1}\n
-	2) ${board.A2} | ${board.B2} | ${board.C2}\n
-	3) ${board.A3} | ${board.B3} | ${board.C3}\n`);
+		console.log(`${board.top}\n
+		1) ${board.A1} | ${board.B1} | ${board.C1}\n
+		2) ${board.A2} | ${board.B2} | ${board.C2}\n
+		3) ${board.A3} | ${board.B3} | ${board.C3}\n`);
 	}
 	
 	function moveLookup() {
-	move = prompt(`${turn}'s move`).toUpperCase();
-	if (move in board && board[move]) {
-		board[move] = turn;
-	} else {
-		console.log('Nope');
-		moveLookup();
-	}
+		client.on("message", message => {
+			var input = message.content.toUpperCase();
+			if (input in board && board[input]) {
+				board[input] = turn
+			} else {
+				message.channel.send('Try again')
+			}
+		}
+		// (`${turn}'s move`).toUpperCase();
+		// if (move in board && board[move]) {
+		// 	board[move] = turn;
+		// } else {
+		// 	console.log('Nope');
+		// 	moveLookup();
+		// }
+		)
 	}
 	
 	function switchTurn() {
-	if (turn === 'X') {
-		turn = 'O';
-	} else {
-		turn = 'X';
-	}
+		if (turn === 'X') {
+			turn = 'O';
+		} else {
+			turn = 'X';
+		}
 	}
 	
 	function play() {
-	while (score.X !== numGames && score.O !== numGames) {
-		newGame();
-		while (winner !== 'x' && winner !== 'o' && winner !== 't') {
-		drawBoard();
-		moveLookup();
-		switchTurn();
-		checkWin();
+		while (score.X !== numGames && score.O !== numGames) {
+			newGame();
+			while (winner !== 'x' && winner !== 'o' && winner !== 't') {
+			drawBoard();
+			moveLookup();
+			switchTurn();
+			checkWin();
+			}
 		}
-	}
 	}
 	
 	function any(arr, val) {
-	return arr.some(arrVal => val === arrVal);
+		return arr.some(arrVal => val === arrVal);
 	}
 	
 	function checkWin() {
@@ -124,6 +159,8 @@ bot.on("message", (message) =>
 			console.log(`X wins: ${score.X} | O wins: ${score.O} | Ties: ${score.T}`);
 		}
 	}
+	game();
+}
 	
-	bot.login(auth.token); //insert bot login token here which can be found at https://discordapp.com/developers/docs/intro
-});
+	
+client.login(auth.token); //insert bot login token here which can be found at https://discordapp.com/developers/docs/intro

d777b193b2f82a4a074acbbb4adaaf6bfc66a43c
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2018-07-23 02:48:37 -0700

    added 8ball

---
+++ cmds/8ball.js
@@ -0,0 +1,36 @@
+const ball = '🎱';
+const answers = [
+    'It is certain', 
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes - definitely',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most likely',
+    'Outlook good',
+    'Yes',
+    'Signs point to yes',
+    'Reply hazy',
+    'Ask again later. Like much later',
+    "Better not tell you now, you'll do something stupid",
+    'Cannot predict now',
+    "Don't count on it",
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+    'What a stupid question',
+    'Stop please',
+    ball,
+    'Insert dank meme',
+    "Maybe when you're older"
+]
+
+module.exports.run = async(client, message, args) => {
+    let randomNum = Math.floor(Math.random() * answers.length)
+    message.channel.send(`${ball}: ${answers[randomNum]}`)
+}
+
+module.exports.help = {
+    name: '8ball'
+}

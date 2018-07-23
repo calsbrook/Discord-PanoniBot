6435d6eb46f90293167b839133bef5a979982583
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2018-07-23 00:08:41 -0700

    game works as long as you enter valid moves

--- cmds/ticTacToe.js
+++ cmds/ticTacToe.js
@@ -10,29 +10,14 @@ module.exports.run = async(client, message, args) => {
 	
 	function game(message) {
 		init();
-		// let p1 = message.member.nickname
-		// message.channel.send(`Tic tac toe started \nPlayer 1: ${p1}\nWho is player 2?`)
-		// message.channel.send('How many games?')
-		// message.channel.awaitMessages(msg => {
-		// 	if (msg.author.bot) return
-		// 	console.log(msg.content)
-		// 	return;
-		// }, {time: 5000})
 		newGame();
-		// numGames = 1;
 		drawBoard(message)
-		message.channel.awaitMessages(msg => {
-			if (msg.author.bot) return;
-			play(msg)
-		})
-		// play(message);
-		
-		// message.channel.awaitMessages(msg => {
-		// 	if (msg.author.bot) return;
-			
-		// 	console.log(msg.content)
-		// })
-		// console.log('game running')
+		if (!winner) {
+			message.channel.awaitMessages(msg => {
+				if (msg.author.bot) return;
+				play(msg)
+			})
+		}
 	}
 	
 	function init() {
@@ -55,34 +40,18 @@ module.exports.run = async(client, message, args) => {
 	}
 	
 	function drawBoard(message) {
-		console.log(`draw board`)
 		message.channel.send(`${board.top}\n𝟷 ${board.A1} | ${board.B1} | ${board.C1}\n𝟸 ${board.A2} | ${board.B2} | ${board.C2}\n3 ${board.A3} | ${board.B3} | ${board.C3}`)
 	}
 	
 	function moveLookup(move) {
 		let moveUpper = move.content.toUpperCase()
-
-		console.log(`move lookup: ${move}`)
-		console.log(move)
-		// client.on("message", message => {
-		// 	var input = message.content.toUpperCase();
-		// 	if (input in board && board[input]) {
-		// 		board[input] = turn
-		// 	} else {
-		// 		message.channel.send('Try again')
-		// 	}
-		// }
-
-		// (`${turn}'s move`).toUpperCase();
 		if (moveUpper in board && board[moveUpper]) {
 			board[moveUpper] = turn;
 			return true
 		} else {
 			move.message.channel.send('Nope');
 			return false
-			// console.log('nopes')
 		}
-		// )
 	}
 	
 	function switchTurn() {
@@ -94,39 +63,13 @@ module.exports.run = async(client, message, args) => {
 	}
 	
 	function play(message) {
-		// console.log(`play start, message.author.bot:`)
-		// console.log(message.author.bot)
-		// while (score.X !== numGames && score.O !== numGames) {
-		// 	newGame();
-		// 	while (winner !== 'x' && winner !== 'o' && winner !== 't') {
-		// 		message.channel.awaitMessages(msg => {
-		// 			console.log(msg)
-		// 			drawBoard(message);
-		// 			if (msg.author.bot) return;
-		// 			moveLookup(msg);
-		// 			switchTurn();
-		// 			checkWin();
-		// 		})
-		// 	}
-		// }
 		if (!winner) {
-			console.log('play start');
 			if (moveLookup(message)) {
 				drawBoard(message)
 				checkWin(message);
 				switchTurn();
 			}
-			// message.channel.awaitMessages(msg => {
-			// 	if (msg.author.bot) return;
-			// 	moveLookup(msg.content)
-				// switchTurn();
-				// checkWin(message);
-				// play(message)
-			// })
-		} else {
-			message.channel.send('Bye')
 		}
-
 	}
 	
 	function any(arr, val) {
@@ -145,17 +88,11 @@ module.exports.run = async(client, message, args) => {
 			board.A3 + board.B2 + board.C1,
 		];
 		if (any(winningCombos, '❌❌❌') || any(winningCombos, '⭕⭕⭕')) {
-			drawBoard(message);
 			winner = turn;
-			score[winner] += 1;
-			console.log(`${winner.toUpperCase()} wins!`);
-			console.log(`X wins: ${score.X} | O wins: ${score.O} | Ties: ${score.T}`);
-		} else if (!any(Object.values(board), ' ')) {
-			drawBoard(message);
+			message.channel.send(`${winner} wins!`)
+		} else if (!any(Object.values(board), '🔲')) {
 			winner = 'T';
-			score.t += 1;
-			console.log('Looks like we got a tie');
-			console.log(`X wins: ${score.X} | O wins: ${score.O} | Ties: ${score.T}`);
+			message.channel.send(`Who could have predicted a tie?`)
 		}
 	}
 }

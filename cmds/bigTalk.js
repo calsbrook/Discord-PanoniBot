1e6d3e7965c9e14ac3157dd293132d84a78be1fc
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2020-06-09 13:32:27 -0700

    restructured to allow emotes and numbers

--- cmds/bigTalk.js
+++ cmds/bigTalk.js
@@ -1,30 +1,36 @@
+const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
 module.exports.run = async(client, message, args) => {
-    let bigMessage = ''
-    let splitMessage = ''
+    let bigMessage = []
+    let splitMessage = []
+
+    for (i=0; i<args.length; i++){
+        if(args[i].search('<:') === 0 || args[i].search(':') === 0) {
+            splitMessage.push(args[i])
+        } else {
+            let bigString = ''
+            for (j=0; j<args[i].length; j++) {
+                if (/^[a-zA-Z]+$/.test(args[i][j])){
+                    bigString += ":regional_indicator_" + args[i][j] + ":"
+                } else if (/^\d+$/.test(args[i][j])){
+                    bigString += `:${digits[parseInt(args[i][j])]}:`
+                } else if (args[i][j] === '!'){
+                    bigString += ':exclamation:'
+                } else if (args[i][j] === '?'){
+                    bigString += ':question:'
+                }
+                else {
+                    bigString += args[i][j]
+                }
+            }
+            if (i<args.length) bigString += '   '
+            splitMessage.push(bigString)
+        }
+    }
     
-    for (i=0; i<args.length;i++) {
-        splitMessage += args[i]
-        splitMessage += ' '
+    for (i=0; i<splitMessage.length; i++) {
+        bigMessage += splitMessage[i]
     }
-
-    for (i=0;i<splitMessage.length;i++) {
-        if (/^[a-zA-Z]+$/.test(splitMessage[i])){
-            bigMessage += ":regional_indicator_" + splitMessage[i].toLowerCase() + ':'
-        }
-        else if (/\s/.test(splitMessage[i])) {
-            bigMessage += '   '
-        }
-        else if (splitMessage[i] === '!'){
-            bigMessage += ':exclamation:'
-        } 
-        else if(splitMessage[i] === '?'){
-            bigMessage += ':question:'
-        }
-        else {
-            bigMessage += splitMessage[i]
-        }
-    }
-
     message.channel.send(bigMessage)
 }
 
